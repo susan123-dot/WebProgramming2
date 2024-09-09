96fafaeb63e37f6beadb4bf117d2632454a8c982
Susan Rajbahak <rajbahaksusan118@gmail.com>
2024-09-09 10:49:10 +0545

    Hello World

---
+++ prg-2/src/main/java/com/susan/prg2/Prg2Application.java
@@ -0,0 +1,13 @@
+package com.susan.prg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prg2Application.class, args);
+	}
+
+}
---
+++ prg-2/src/test/java/com/susan/prg2/Prg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.susan.prg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prg2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
