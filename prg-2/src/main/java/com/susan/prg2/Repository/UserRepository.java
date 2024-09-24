807498aa919bdf0e2ece384711483e060990e041
Susan Rajbahak <rajbahaksusan118@gmail.com>
2024-09-24 21:15:01 +0545

    add new page

---
+++ prg-2/src/main/java/com/susan/prg2/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.susan.prg2.Repository;
+
+import com.susan.prg2.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}
